516a7aa77289a14b6d85b690a9493a9dddfde8ad
SomeDudRice <Lucasl197@nycstudents.net>
2026-03-06 10:10:16 -0500

    Update Emusaresatanincarnate.py

--- Emusaresatanincarnate.py
+++ Emusaresatanincarnate.py
@@ -1,9 +1,7 @@
-
 items = {
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
 }
-for index, item in enumerate("bestbuy"):
-    print(index, ":", (item["name"]))
+print (items[0]["name"])
