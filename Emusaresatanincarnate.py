f14bb74dc8db5d19780851c819b2d2b703361c9e
SomeDudRice <Lucasl197@nycstudents.net>
2026-03-19 10:17:05 -0400

    Update Emusaresatanincarnate.py

--- Emusaresatanincarnate.py
+++ Emusaresatanincarnate.py
@@ -1,28 +1,43 @@
 items = [
  {
-   "name": "baseball",
-     "price": 5,
-     "weight": 150,},
+   "name": "base ball",
+   "price": 5,
+   "weight": 150,
+ },
  {
    "name": "basket ball",
-     "price": 20,
-     "weight": 600,},
+    "price": 20,
+    "weight": 600,
+ },
  {
    "name": "volley ball",
-     "price": 25,
-     "weight": 400,},
+   "price": 25,
+   "weight": 400,
+ },
  {
    "name": "bowling ball",
-     "price": 75,
-     "weight": 7000,},
+   "price": 75,
+   "weight": 7000,
+ },
  {
    "name": "tennis ball",
-     "price": 2,
-     "weight": 60,},
+   "price": 1,
+   "weight": 60,
+ },
  {
    "name": "soccer ball",
-     "price": 50,
-     "weight": 450,},
+   "price": 50,
+   "weight": 450,
+ }
 ]
-for index, items in enumerate(items):
-    print(index, ":", items["name"])
+order = 0
+cart = []
+for index, item in enumerate(items):
+  print(index, ":", item["name"])
+buying=0
+while buying == 0:
+    order = int(input("which one"))
+    print ("would you like to buy",items[order]["name"],items[order]["price"],items[order]["weight"])
+    if input("type 1 to buy, 2 to cancel") == 1:
+     cart.append(item[order])
+     
