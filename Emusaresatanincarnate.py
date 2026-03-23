5decdcd891f86dfa0323d94816dff3c3733ee60f
SomeDudRice <Lucasl197@nycstudents.net>
2026-03-23 10:03:20 -0400

    Update Emusaresatanincarnate.py

--- Emusaresatanincarnate.py
+++ Emusaresatanincarnate.py
@@ -30,14 +30,19 @@ items = [
    "weight": 450,
  }
 ]
+total =0
 order = 0
+corder = 0
 cart = []
 for index, item in enumerate(items):
   print(index, ":", item["name"])
 buying=0
 while buying == 0:
-    order = int(input("which one"))
-    print ("would you like to buy",items[order]["name"],items[order]["price"],items[order]["weight"])
-    if input("type 1 to buy, 2 to cancel") == 1:
-     cart.append(item[order])
-     
+  order = int(input("which one"))
+  print ("would you like to buy",items[order]["name"],",",items[order]["price"],"$,",items[order]["weight"],"grams")
+  if int(input("type 1 to buy, 2 to cancel")) == 1:
+    cart.append(items[order]['name'])
+    total = total + items[order]["price"]
+  if int(input("would you like to continue shopping? 1 to continue, 2 to end")) == 2:
+    buying= +1
+print("youve pruchased",cart, "price is",total)
